9a459a577268179623a78f6c25cdef1f741201ff
leod <subtle.frustration@proton.me>
2018-03-01 23:24:53 +0100

    Fix angle bug in cone spawning

--- src/spawn.rs
+++ src/spawn.rs
@@ -1,11 +1,8 @@
 use rand::{self, Rng};
 use gfx;
 
-use {System, Particle};
+use {Particle, System};
 
-/// Spawn a bunch of particles in `orientation` (radians), spread out by `spread` (radians).
-/// The speed is chosen uniformly between `min_speed` and `max_speed`. The color of the particle
-/// can be calculated depending on the angle and the speed with the `color` function.
 pub struct Cone<F> {
     pub spawn_time: f32,
     pub life_time: f32,
@@ -14,23 +11,29 @@ pub struct Cone<F> {
     pub spread: f32,
     pub min_speed: f32,
     pub max_speed: f32,
-    pub angle: f32, 
+    pub angle: f32,
     pub friction: f32,
     pub size: [f32; 2],
     pub color: F,
 }
 
 impl<F: Fn(f32, f32) -> [f32; 3]> Cone<F> {
+    /// Spawn a bunch of particles in `orientation` (radians), spread out by `spread` (radians).
+    /// The speed is chosen uniformly between `min_speed` and `max_speed`. The color of the particle
+    /// can be calculated depending on the angle and the speed with the `color` function.
     pub fn spawn<R: gfx::Resources>(&self, system: &mut System<R>, n: usize) {
         let mut rng = rand::thread_rng();
 
         for _ in 0..n {
-            let angle = rng.gen_range(self.orientation - self.spread, self.orientation + self.spread);
+            let angle = rng.gen_range(
+                self.orientation - self.spread,
+                self.orientation + self.spread,
+            );
             let speed = rng.gen_range(self.min_speed, self.max_speed);
-            let vel = [self.angle.cos() * speed, self.angle.sin() * speed];
+            let vel = [angle.cos() * speed, angle.sin() * speed];
             let color = (self.color)(angle, speed);
             let particle = Particle {
-                spawn_time: self.spawn_time,            
+                spawn_time: self.spawn_time,
                 life_time: speed / self.friction,
                 pos: self.pos,
                 vel,
